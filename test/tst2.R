ffb6943d3f7ac7bb75ae28dd12b4f11795e69328
aju <aju@te.ceaaces>
2014-07-28 17:23:44 -0500

    Including example with two different kernels.

---
+++ test/tst2.R
@@ -0,0 +1,12 @@
+#___________________________________________________________________________________________________
+library(RKHSENS)
+options( stringsAsFactors = FALSE )
+
+#___________________________________________________________________________________________________
+kernel_1<-function( x, y ) min(x,y)
+kernel_2<-function( x, y ) exp(-x*y)
+
+kernels<-data.frame( id = 1:2, kern = c( 'kernel_1', 'kernel_2' ), linf = c(0,-1), lsup = c(1,1) )
+X<-data.frame( x1 = seq(0,1,length.out=100), x2 = seq(-1,1,length.out=100) )
+Y<-array_integrate_kern( kernels, X )
+alpha<-kern_integral( kernels )
