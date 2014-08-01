394ef51d3f95aa39da274dc324cb7ad8f4c2b05f
aju <aju@te.ceaaces>
2014-08-01 14:18:55 -0500

    Using exponencial kernel.

--- test/tst2.R
+++ test/tst2.R
@@ -3,8 +3,8 @@ library(RKHSENS)
 options( stringsAsFactors = FALSE )
 
 #___________________________________________________________________________________________________
-kernel_1<-function( x, y ) min(x,y)
-kernel_2<-function( x, y ) exp(-x*y)
+kernel_1<-function( x, y ) exp( -0.2*(x-y)^2)
+kernel_2<-function( x, y ) exp( -0.2*(x-y)^2)
 
 kernels<-data.frame( id = 1:2, kern = c( 'kernel_1', 'kernel_2' ), linf = c(0,-1), lsup = c(1,1) )
 X<-data.frame( x1 = seq(0,1,length.out=10), x2 = seq(-1,1,length.out=10) )
@@ -13,3 +13,8 @@ alpha<-kern_integral( kernels )
 
 # Returns Gamma array and KANOVA matrix
 GK<-eval_kernels( kernels, X, I, alpha )
+C<-chol( GK$KANOVA )
+C<-chol2inv( C )
+
+# E<-eigen( GK$KANOVA )
+# E$values
