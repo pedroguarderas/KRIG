8bd6bfef8fcafd0f55551d38819e4ffec02c4c9a
aju <aju@te.ceaaces>
2014-08-01 19:20:31 -0500

    Improving example in tst2.R

--- test/tst2.R
+++ test/tst2.R
@@ -3,17 +3,18 @@ library(RKHSENS)
 options( stringsAsFactors = FALSE )
 
 #___________________________________________________________________________________________________
+N<-50
 kernel_1<-function( x, y ) exp( -0.5*(x-y)^2)
 kernel_2<-function( x, y ) exp( -0.7*(x-y)^2)
 kernel_3<-function( x, y ) exp( -0.1*(x-y)^2)
 
 kernels<-data.frame( id = 1:3, 
                      kern = c( 'kernel_1', 'kernel_2', 'kernel_3' ), 
-                     linf = c(0,-1, 0 ), 
-                     lsup = c(1,1,2) )
-X<-data.frame( x1 = runif( 30, 0, 1 ), 
-               x2 = runif( 30, -1, 1 ),
-               x3 = runif( 30, 0, 2 ))
+                     linf = c( 0, -1, -5 ), 
+                     lsup = c( 1, 1, 5 ) )
+X<-data.frame( x1 = runif( N, 0, 1 ), 
+               x2 = runif( N, -1, 1 ),
+               x3 = runif( N, -5, 5 ) )
 I<-array_integrate_kern( kernels, X )
 alpha<-kern_integral( kernels )
 
