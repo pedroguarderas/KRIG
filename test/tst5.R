da74507d7362ad8e5c5b969a6d60dfc57b8e8147
pedroguarderas <pedro.guarderas@webrisko.com>
2017-10-17 23:41:36 -0500

    Enabling example for ordinary Gaussian regression.

--- test/tst5.R
+++ test/tst5.R
@@ -1,33 +1,42 @@
-#___________________________________________________________________________________________________
-# Kriging ordinary example in 3D
-library(rgl)
-library(RKHSENS)
+# Example 5 ----------------------------------------------------------------------------------------
+# Gaussian process regression
+library( RKHSENS )
+library( rgl )
 
-
-m<-40
-x<-seq(-2,5,length.out=m)
+# Observed -----------------------------------------------------------------------------------------
+m<-50
+x<-seq( -3, 6, length.out = m )
 y<-x
 f<-function(x,y){
-  return(1+exp(-x^2-y^2)+0.5*exp(-(x-2)^2-(y-2)^2) - 0.5*exp(-(x-4)^2-(y-0.25)^2)) 
+  return(1 + exp(-x^2-y^2)+0.5*exp(-(x-2)^2-(y-2)^2) - 0.5*exp(-(x-4)^2-(y-0.25)^2)) 
 }
 z<-outer(x,y,f)
 
-n<-30
-x1<-runif( n, -2, 5 )
-x2<-runif( n, -2, 5 )
+# Prediction ---------------------------------------------------------------------------------------
+n<-40
+x1<-runif( n, -3, 6 )
+y1<-runif( n, -3, 6 )
+X<-cbind(x1,y1)
 
-X<-cbind(x1,x2)
-x0<-as.matrix(expand.grid(x,y))
+m<-50
+x0<-seq( -3, 6, length.out = m )
+y0<-x0
+Y<-as.matrix( expand.grid( x0, y0 ) )
+
 Z<-NULL
-for(i in 1:n) Z<-c(Z,f(x1[i],x2[i]))
+for(i in 1:n) Z<-c( Z, f(x1[i],y1[i]) )
+Z<-as.matrix( Z, n, 1 )
 
+# Kernel -------------------------------------------------------------------------------------------
 s<-10.0
 t<-1.1
-k<-function( x, y ) return( Ker_Exp( x, y, s, t ) )
-krgs<-kriging_ordinary( Z, X, x0, k )
-Z0<-matrix( krgs$Z0, m, m )
+Kern<-function( x, y ) return( RKHKerExp( sum( (x-y)^2 ), s, t ) )
 
+# Gaussian process estimation ----------------------------------------------------------------------
+krgs<-RKHEstimate( Z, X, Y, Kern, 1 )
+W<-matrix( krgs$W, m, m )
 
-persp3d( x, y, Z0, col='darkgreen', alpha = 1.0 )
+# Plotting the results -----------------------------------------------------------------------------
+persp3d( x0, y0, W, col='darkgreen', alpha = 0.8 )
 persp3d( x, y, z, col='gold', alpha = 0.6, add = TRUE )
-points3d( x1, x2, Z, size = 8.0, col = 'purple', add = TRUE )
+points3d( x1, y1, Z, size = 8.0, col = 'dodgerblue3', add = TRUE )
