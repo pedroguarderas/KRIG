e93f05232759c8b8bc3f1f629037191d224cfcee
pedroguarderas <pedro.guarderas@webrisko.com>
2018-01-15 23:28:23 -0500

    Description of mining data.

---
+++ R/mining.R
@@ -0,0 +1,17 @@
+#' Mining data
+#' @docType data
+#' @usage data( 'Copper', packages = 'KRIG' )
+#' @keywords kriging
+#' @references Clark
+#' @examples 
+#' data( 'Copper', packages = 'KRIG' )
+"Copper"  
+
+#' Mining data
+#' @docType data
+#' @usage ddata( 'BM', packages = 'KRIG' )
+#' @keywords kriging
+#' @references Clark
+#' @examples 
+#' data( 'BM', packages = 'KRIG' )
+"BM"  
