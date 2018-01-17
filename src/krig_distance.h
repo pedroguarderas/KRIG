51b05f6334551907b48c8d50581c2c16ea172d85
pedroguarderas <pedro.guarderas@webrisko.com>
2018-01-17 01:16:38 -0500

    Improving documentation of weighthed distance with different powers.

--- src/krig_distance.h
+++ src/krig_distance.h
@@ -14,13 +14,13 @@ using namespace Rcpp;
 //' @title Generic weighted p-distance
 //' @description Many isotropic models can be defined employing a distance. In particular this 
 //' function implements a distance employing weights and different powers.
-//' @param x First vector
-//' @param y Second vector
-//' @param w Weights for every coordinate in the vectors
+//' @param x First vector.
+//' @param y Second vector.
+//' @param w Weights for every coordinate in the vectors.
 //' @param p Powers for every coordinate in the vectors. To define a true norm every coordinate
 //' has to be greater than 1.
-//' @return Numeric value of the weighted p-distance.
-//' @author Pedro Guarderas
+//' @return Real value of the weighted p-distance.
+//' @author Pedro Guarderas \email{pedro.felipe.guarderas@@gmail.com}.
 //' @export
 // [[Rcpp::export]]
 double weight_pow_dist( const arma::colvec& x, 
