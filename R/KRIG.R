62cdaf189149e65260bd46ec33324cd4c92f612a
pedroguarderas <pedro.guarderas@webrisko.com>
2018-01-27 04:31:10 -0500

    Updating documentation.

--- R/KRIG.R
+++ R/KRIG.R
@@ -1,8 +1,8 @@
 # KRIG ---------------------------------------------------------------------------------------------
-#' @title Spatial statistics with kriging.
+#' @title Spatial Statistics with Kriging.
 #' @description Implements different methods for spatial statistics, in particular focused with 
-#' kriging based models. We count with different implemented models, simple, ordinary and 
-#' universal forms of kriging, cokriging and regression kriging models. Includes, multivariate 
+#' Kriging based models. We count with different implemented models, simple, ordinary and 
+#' universal forms of Kriging, co-Kriging and regression Kriging models. Includes, multivariate 
 #' sensitivity analysis under an approximation designed over Reproducing Kernel Hilbert Spaces and 
 #' computation of Sobol indexes under this framework.
 #' 
